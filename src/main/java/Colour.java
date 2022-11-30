26993578518b10510e40f31b58290f85fed2db7d
Lyonstead <TommyLyons02@gmail.com>
2022-11-30 23:11:28 +0000

    Added data validation for 24 bit constructor

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -11,9 +11,9 @@ public class Colour {
             red < 0.0f && green < 0.0f && blue < 0.0f) {
             throw new IllegalArgumentException("Input variables are not within Bounds");
         }
-        if (    ((Object)red).getClass().getSimpleName().equals("Float") &&
-                ((Object)blue).getClass().getSimpleName().equals("Float") &&
-                ((Object)green).getClass().getSimpleName().equals("Float")) {
+        if (    !((Object)red).getClass().getSimpleName().equals("Float") &&
+                !((Object)blue).getClass().getSimpleName().equals("Float") &&
+                !((Object)green).getClass().getSimpleName().equals("Float")) {
             throw new IllegalArgumentException("Invalid input type");
         }
         this.red = (int) (red * 255);
@@ -23,10 +23,18 @@ public class Colour {
     }
 
     public Colour(String rgb) {
-        this.rgb = rgb;
-        this.red = Integer.parseInt(rgb.substring(0,8),2);
-        this.green = Integer.parseInt(rgb.substring(8,16), 2);
-        this.blue = Integer.parseInt(rgb.substring(16,24), 2);
+        if (rgb.length() != 24) {
+            throw new IllegalArgumentException("Argument must be a 24 bit address");
+        }
+        for (int i = 0; i < rgb.length(); i++) {
+            if (!(rgb.substring(i, i+1).equals("1") || rgb.substring(i, i+1).equals("0"))) {
+                throw new IllegalArgumentException("Argument must consist of 1s or 0s");
+            }
+            this.rgb = rgb;
+            this.red = Integer.parseInt(rgb.substring(0, 8), 2);
+            this.green = Integer.parseInt(rgb.substring(8, 16), 2);
+            this.blue = Integer.parseInt(rgb.substring(16, 24), 2);
+        }
     }
 
 }
