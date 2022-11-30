39b399f9070118430c33e896393b2f16b3a00bd1
Lyonstead <TommyLyons02@gmail.com>
2022-11-30 16:17:31 +0000

    Constructors for Colour

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -5,10 +5,15 @@ public class Colour {
     int blue;
 
     public Colour(float red, float green, float blue) {
+        this.red = (int) (red * 255);
+        this.green = (int) (green * 255);
+        this.blue = (int) (blue * 255);
     }
 
     public Colour(String rgb) {
-
+        this.red = Integer.parseInt(rgb.substring(0,8),2);
+        this.green = Integer.parseInt(rgb.substring(8,16), 2);
+        this.blue = Integer.parseInt(rgb.substring(16,24), 2);
     }
 
 }
